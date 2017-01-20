70453b9e988143fda51dc3bcad323f49f1f11cd9
MatsLP <lukas.plogmacher@gmail.com>
2017-01-20 16:52:10 +0100

    Add files via upload

--- c_module.cpp
+++ c_module.cpp
@@ -1,10 +1,10 @@
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-#include <Python.h>
-#include <arrayobject.h>
-#include <vector>
-#include <boost/numeric/odeint.hpp>
+#include <Python.h> //must be included 
+#include <arrayobject.h>//must be included to access numpy arrays
+#include <vector>//C++
+#include <boost/numeric/odeint.hpp>//C++ numeric library
 
-
+//These two classes are needed to use the boost odeint library
 class observer_class{
     int i;
     PyArrayObject * write_array;
@@ -14,7 +14,7 @@ class observer_class{
                 }
     void operator() (const std::vector<double> state, double t ){
         for (int k=0; k<state.size(); k++)
-            *(double*)PyArray_GETPTR2(write_array, i, k) = state[k];
+            *(double*)PyArray_GETPTR2(write_array, i, k) = state[k]; // write into Numpy arrays
         i++;
     }
 };
@@ -29,7 +29,7 @@ class lorentz_class{
             R = in_R;
             b = in_b;
         }
-        void operator() (const std::vector<double> & x, std::vector<double> & dxdt, const double t){
+        void operator() (const std::vector<double> & x, std::vector<double> & dxdt, const double t){ //lorentz system
             dxdt[0] = sigma * ( x[1] - x[0] );
             dxdt[1] = R * x[0] - x[1] - x[0] * x[2];
             dxdt[2] = -b * x[2] + x[0] * x[1];
@@ -38,6 +38,7 @@ class lorentz_class{
 };
 
 static PyObject* solve (PyObject *dummy, PyObject *args){
+// declare Variables
     PyObject *out_object;
     PyObject *input_dict;
     PyArrayObject * out_array=NULL;
@@ -49,8 +50,10 @@ static PyObject* solve (PyObject *dummy, PyObject *args){
     int numsteps;
     std::vector<double> current_state (4);
 
+
+// Translate Pythonobjects into C-types
     if (!PyArg_ParseTuple(args, "OO", &out_object, &input_dict)) return NULL;
-    out_array = (PyArrayObject*)PyArray_FROM_OTF(out_object, NPY_DOUBLE, NPY_ARRAY_INOUT_ARRAY); // increases reference count
+    out_array = (PyArrayObject*)PyArray_FROM_OTF(out_object, NPY_DOUBLE, NPY_ARRAY_INOUT_ARRAY); // increases reference count +1
 
     if (NULL != PyDict_GetItemString(input_dict, "sigma"))
         sigma = PyFloat_AsDouble(PyDict_GetItemString(input_dict, "sigma"));
@@ -68,6 +71,7 @@ static PyObject* solve (PyObject *dummy, PyObject *args){
         numsteps = PyLong_AsLong(PyDict_GetItemString(input_dict, "numsteps"));
     else goto fail;
 
+//it is pure C++ from here on
     lorentz_class lorentz;
     lorentz.init(sigma, R, b);
 
@@ -75,31 +79,31 @@ static PyObject* solve (PyObject *dummy, PyObject *args){
     observer.init(out_array);
 
     for (int i=0; i<4; i++)
-        current_state[i] = *(double*)PyArray_GETPTR2(out_array, 0, i);
+        current_state[i] = *(double*)PyArray_GETPTR2(out_array, 0, i); // initialize C Array with entries from Python Object
     boost::numeric::odeint::integrate_n_steps(boost::numeric::odeint::make_dense_output( 1.0e-12 , 1.0e-12 , boost::numeric::odeint::runge_kutta_dopri5< std::vector<double> >() ),
                                             lorentz, current_state, 0., dt, numsteps, observer);
     Py_DECREF(out_array); // does not matter if i decrease this or PyObject out_object
-    Py_INCREF(Py_None);
-    return Py_None;
+    Py_INCREF(Py_None); // refcount of Py_None will be lowered by one when returned. Therefore we need to increment it by one
+    return Py_None; //return Py_None to indicate success
 
     fail:
-        PyErr_SetString(PyExc_KeyError, "parameters not specified correctly");
-        Py_XDECREF(out_array);
-        return NULL;
+        PyErr_SetString(PyExc_KeyError, "parameters not specified correctly"); //set nice Errormessage
+        Py_XDECREF(out_array);//decrease Reference count of out_array. decreasing out_object would be identical
+        return NULL; //return NULL to indicate Error
 }
 
 // this defines docstring, names of methods and type of arguments when calling
 static PyMethodDef mymethods[] = {
-    { "solve",solve,
-      METH_VARARGS,
-      "solve ode"},
+    { "solve",solve, //name, c-funtion
+      METH_VARARGS, // how to call it from Python
+      "solve ode"}, // doc string
     {NULL, NULL, 0, NULL} /* Sentinel */
     };
 
 
 PyMODINIT_FUNC
-initlorentz(void) // name must correspond to final file name
+initlorentz(void) // name must "init" + "module_name"
 {
-   (void)Py_InitModule("lorentz", mymethods);
-   import_array();
+   (void)Py_InitModule("lorentz", mymethods); // name of module must coincide with name two lines above
+   import_array(); //needed to access Numpy-arrays
 }
